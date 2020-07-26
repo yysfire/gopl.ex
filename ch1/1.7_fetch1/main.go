da9274c6da35c73469eda54efaebe1ac54ff62b8
yysfire <yysfire+repo@gmail.com>
2020-07-26 19:18:45 +0800

    Add codes of exercise 1.7, 1.8, 1.9

---
+++ ch1/1.7_fetch1/main.go
@@ -0,0 +1,36 @@
+// 《The Go Programming Language》: http://gopl.io/
+// 《Go语言圣经》: https://github.com/golang-china/gopl-zh
+// Copyright © 2020 yysfire. All Rights Reserved.
+
+// 练习 1.7：
+// 函数调用io.Copy(dst, src)会从src中读取内容，并将读到的结果写入到dst中，
+// 使用这个函数替代掉例子中的ioutil.ReadAll来拷贝响应结构体到os.Stdout，
+// 避免申请一个缓冲区（例子中的b）来存储。记得处理io.Copy返回结果中的错误。
+
+// Fetch prints the content found at each specified URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		defer resp.Body.Close()
+		_, err = io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
+
+//!-
---
+++ ch1/1.8_fetch2/main.go
@@ -0,0 +1,45 @@
+// 《The Go Programming Language》: http://gopl.io/
+// 《Go语言圣经》: https://github.com/golang-china/gopl-zh
+// Copyright © 2020 yysfire. All Rights Reserved.
+
+// 练习 1.8：
+// 修改fetch这个范例，如果输入的url参数没有 http:// 前缀的话，
+// 为这个url加上该前缀。你可能会用到strings.HasPrefix这个函数。
+
+// Fetch prints the content found at each specified URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !(strings.HasPrefix(url, "http://") || strings.HasPrefix(url, "https://")) {
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			url = strings.Replace(url, "http://", "https://", 1)
+			fmt.Println("try to fetch:", url)
+			resp, err = http.Get(url)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+				os.Exit(1)
+			}
+		}
+		defer resp.Body.Close()
+		_, err = io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
+
+//!-
